2372079af96bdcd999304f9c12f339576acdd5dd
João Vitor Averaldo Antunes <joaoaveraldo@yahoo.com.br>
2026-07-10 15:11:22 -0300

    cleanup

--- minecraft-mod/src/main/java/com/visomod/export/TestResource.java
+++
@@ -1,7 +0,0 @@
-package com.visomod.export;
-public class TestResource {
-    public static void test() throws Exception {
-        java.lang.reflect.Field f = net.minecraft.client.renderer.texture.SpriteContents.class.getDeclaredField("byMipLevel");
-        System.out.println("Field found: " + f.getName());
-    }
-}
